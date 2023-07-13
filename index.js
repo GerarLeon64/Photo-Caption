07e2142d2ada89bcf617a61220938ce733469d8c
Gerardo Leon <gleonnyc@gmail.com>
2023-07-13 09:59:43 -0400

    added user, image, and caption models to database

--- index.js
+++ index.js
@@ -25,15 +25,65 @@ const User = sequelize.define('user', {
     timestamps: false
 });
 
-User.sync( { alter: true }).then(() => {
+const Image = sequelize.define('image', {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    captionId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
+const Caption = sequelize.define('caption', {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+    imageId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true
+    },
+    descripton: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+// setting up one-to-many and one-to-one relationships
+User.hasMany(Caption, {
+    foreignKey: 'userId'
+});
+Caption.belongsTo(User);
+
+Caption.hasOne(Image, {
+    foreignKey: 'captionId'
+});
+Image.belongsTo(Caption);
+
+Image.sync( { alter: true }).then(() => {
     // working with our updated table
-    return User.create({
-        username: 'Jimmy',
-        password: '456'
-    });
+    return Image.findAll();
 }).then((data) => {
-    console.log(data.toJSON());
-    console.log('User added to database!');
+    data.forEach(element => {
+        console.log(element.toJSON());
+    })
 })
 .catch((err) => {
     console.log(err);
